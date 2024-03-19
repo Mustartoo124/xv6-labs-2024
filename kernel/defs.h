c7c7678495c554bc22d1301515efa8d794a6edd1
Mustartoo124 <giabaofinance2022@gmail.com>
2024-03-19 22:06:24 +0700

    fix define and usys bug, completed assignment 2, 3

--- kernel/defs.h
+++ kernel/defs.h
@@ -17,7 +17,7 @@ void            brelse(struct buf*);
 void            bwrite(struct buf*);
 void            bpin(struct buf*);
 void            bunpin(struct buf*);
-int             freemem(void);
+int          freemem(void);
 
 // console.c
 void            consoleinit(void);
